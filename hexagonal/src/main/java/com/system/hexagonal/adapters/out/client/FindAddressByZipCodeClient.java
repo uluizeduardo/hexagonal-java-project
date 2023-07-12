d1f18b06eee5eca30c8b97f7186951dc8bba8c95
uluizeduardo <luiz.analistadesistemas@gmail.com>
2023-07-11 22:59:27 -0300

    updated client url

--- hexagonal/src/main/java/com/system/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
+++ hexagonal/src/main/java/com/system/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
         name = "FindAddressByZipCodeClient",
-        url = "${http://localhost:8082/addresses}"
+        url = "${hexagonal.client.address.url}"
 )
 public interface FindAddressByZipCodeClient {
 
