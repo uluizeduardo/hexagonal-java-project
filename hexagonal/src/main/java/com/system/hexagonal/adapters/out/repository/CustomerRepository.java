3696e1682a110c08baf1f0a8b23425233e72c04b
uluizeduardo <luiz.analistadesistemas@gmail.com>
2023-07-12 21:54:47 -0300

    Created the repository

---
+++ hexagonal/src/main/java/com/system/hexagonal/adapters/out/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.system.hexagonal.adapters.out.repository;
+
+import com.system.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+}
